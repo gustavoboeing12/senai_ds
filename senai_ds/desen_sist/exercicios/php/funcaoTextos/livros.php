5673cbbc69f8b6fa4fc5149b9279e72615747478
gustavoboeing12 <gustavofratonib@gmail.com>
2025-07-29 14:10:02 -0300

    Atualização
    Pasta PHP atualizada

--- senai_ds/desen_sist/exercicios/php/funcaoTextos/livros.php
+++
@@ -1,8 +0,0 @@
-<?php
-    foreach(file("livros.txt") as $livros){
-        list($titulo, $autor) = explode(",", $livros);
-?>
-<p> Título: <?= $titulo ?>, Autor: <?= $autor ?> </p>
-<?php
-   }
-?>
--- senai_ds/desen_sist/exercicios/php/php/phpExterno.php
+++
@@ -1,6 +0,0 @@
-<?php
-         if(isset($_GET['nome'])&& isset($_GET['idade'])){
-            echo "Recebido o cliente". $_GET['nome'];
-            echo "Que tem ".$_GET['idade']. " anos.";
-         }
-     ?>
